c94230d0f943463563813bd964d28ab29465f7b5
Jamshid-net <39515083+jamshid-net@users.noreply.github.com>
2023-07-05 23:54:24 +0500

    Add project files.

---
+++ Domain/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+public class Message
+{
+    public string Title { get; set; }
+    public string ImageName { get; set; }
+    public byte[] ImageByte { get; set; } 
+    
+}
---
+++ Domain/NameTitle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain;
+public class NameTitle
+{
+    public string Title { get; set; }
+    public string Name { get; set; }
+    
+}
